f56a047ffdb02b4288461f2ec3b711ea1152c5b2
demong4 <ganeshhalembre@gmail.com>
2025-03-13 10:23:21 +0000

    first commit PES2UG22CS199

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Iam PES2UG22CS199";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
